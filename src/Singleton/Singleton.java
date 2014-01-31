4e1189cf74f4f3e162a62bb8c5095fb35fefe593
t-aguiar <antoner.aguiar@gmail.com>
2014-01-31 12:47:20 -0800

    Added Singleton pattern

--- src/Singleton/Singleton.java
+++ src/Singleton/Singleton.java
@@ -6,10 +6,20 @@ package Singleton;
  * ie. lists that multiple portions depend on
  *      resource pools
  *      connections
+ *
+ * Multithreading can cause issues, one soln synchronized to static method
+ * High overhead however, only needed on the first path.  Bad idea if class
+ * will get alot of traffic.
+ *
+ * Another solution is to create an instance when program loads class.
+ * Little bit of overhead but likely to be far less than synchronizing
+ *
+ * Option 3: synchronize only the actual instantiation
  */
 public class Singleton {
     //A self reference
-    private static Singleton instance;
+    //in Option3 add 'volatile' to instance declaration
+    private static Singleton instance = new Singleton();
 
     private Singleton ()
     {
@@ -19,11 +29,15 @@ public class Singleton {
     //Derp.
     //forgot that it has to be static to even call it the 1st time
     public static Singleton instance()
-    {
+    {   /*Removed for safer multithreading
         if(instance == null)//if its already created
         {
-            return new Singleton();
-        }
+            synchronized (Singleton.class)
+            {
+                if(instance == null)
+                    instance = new Singleton();
+            }
+        }*/
         return instance;
     }
 
